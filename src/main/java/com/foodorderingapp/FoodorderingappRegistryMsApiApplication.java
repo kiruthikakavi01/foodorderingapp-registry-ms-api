24b5c98c8b8a81b7780c293ac40472e5a356c712
kiruthikakavi01 <kiruthikakavi01@gmail.com>
2022-05-05 16:45:35 +0530

    first commit

---
+++ src/main/java/com/foodorderingapp/FoodorderingappRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodorderingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorderingappRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodorderingappRegistryMsApiApplication.class, args);
+	}
+
+}
